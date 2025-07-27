b94871e3352f0ec7de6dc894872c6648f9539fc0
Artem <alefa2000@mail.ru>
2025-07-27 11:49:51 +0300

    Update EditPageViewModel.cs

--- AvaloniaLearning/ViewModel/EditPageViewModel.cs
+++ AvaloniaLearning/ViewModel/EditPageViewModel.cs
@@ -94,7 +94,7 @@ namespace AvaloniaApp.ViewModel
 
         private void NavigateBack()
         {
-            _navigationService.Navigate<MainPageViewModel>();
+            _navigationService.NavigateBack();
         }
     }
 }
